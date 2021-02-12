687c32b81a346a8bc16dfea9f830f55024694b8b
Hunachi <hanahanahunachi@gmail.com>
2021-02-13 01:41:19 +0900

    morucar side miror

--- morucar.js
+++ morucar.js
@@ -100,6 +100,20 @@ function init() {
     const taiya3 = new THREE.Mesh(gtaiya, mtaiya);
     taiya3.position.set(-70, -60, 200);
     scene.add(taiya3);
+    const gtaiyaInside = new THREE.SphereGeometry(10, 50, 50);
+    const mtaiyaInside = new THREE.MeshBasicMaterial({ color: 0xF0C135 });
+    const taiyaInside0 = new THREE.Mesh(gtaiyaInside, mtaiyaInside);
+    taiyaInside0.position.set(70, -60, 0);
+    scene.add(taiyaInside0);
+    const taiyaInside1 = new THREE.Mesh(gtaiyaInside, mtaiyaInside);
+    taiyaInside1.position.set(-70, -60, 0);
+    scene.add(taiyaInside1);
+    const taiyaInside2 = new THREE.Mesh(gtaiyaInside, mtaiyaInside);
+    taiyaInside2.position.set(70, -60, 200);
+    scene.add(taiyaInside2);
+    const taiyaInside3 = new THREE.Mesh(gtaiyaInside, mtaiyaInside);
+    taiyaInside3.position.set(-70, -60, 200);
+    scene.add(taiyaInside3);
 
     // 目
     const gme = new THREE.SphereGeometry(20, 50, 50);
@@ -136,12 +150,29 @@ function init() {
     // 窓
     const gmadofront = new THREE.SphereGeometry(100, 500, 500, Math.PI / 3 + Math.PI / 9, 2 * Math.PI / 3 - 2 * Math.PI / 9, (Math.PI) / 4, Math.PI / 7);
     const mmadofront = new THREE.MeshBasicMaterial({ color: 0xB48166 });
-    const madofront = new THREE.Mesh(gmadofront, mmadofront);
-    madofront.rotation.y = Math.PI / 3;
-    madofront.rotation.z = Math.PI / 120;
-    madofront.position.set(51, 0, 100);
-    scene.add(madofront);
-    // TODO
+    const madofront0 = new THREE.Mesh(gmadofront, mmadofront);
+    madofront0.rotation.y = Math.PI / 3;
+    madofront0.rotation.z = Math.PI / 120;
+    madofront0.position.set(51, 0, 100);
+    scene.add(madofront0);
+    const madofront1 = new THREE.Mesh(gmadofront, mmadofront);
+    madofront1.rotation.y = Math.PI / 3 + Math.PI;
+    madofront1.rotation.z = Math.PI / 120;
+    madofront1.position.set(-51, 0, 100);
+    scene.add(madofront1);
+    // radiusTop : Float, radiusBottom : Float, height : Float, radialSegments : Integer, heightSegments : Integer, openEnded : Boolean, thetaStart : Float, thetaLength : Float
+    const gmadoSide = new THREE.CylinderGeometry(101, 101, 140, 100, 10, true, 0, Math.PI / 7);
+    const mmadoSide = new THREE.MeshBasicMaterial({ color: 0xB48166 });
+    const madoSide0 = new THREE.Mesh(gmadoSide, mmadoSide);
+    madoSide0.position.set(0, 0, 100)
+    madoSide0.rotation.x = - Math.PI / 10;
+    madoSide0.rotation.z = Math.PI / 2;
+    scene.add(madoSide0);
+    const madoSide1 = new THREE.Mesh(gmadoSide, mmadoSide);
+    madoSide1.position.set(0, 0, 100)
+    madoSide1.rotation.x = Math.PI + Math.PI / 10 + Math.PI / 7;
+    madoSide1.rotation.z = Math.PI / 2;
+    scene.add(madoSide1);
 
     // 口と鼻
     const nosePoints = [];
@@ -179,9 +210,9 @@ function init() {
          camera.position.x = 500 * Math.sin(Date.now() / 1000); //500; //
          camera.position.y = 0;
          camera.position.z = 500 * Math.cos(Date.now() / 1000); //100; //
-        // camera.position.x = 400;//500 * Math.sin(Date.now() / 2000); //500; //
-        // camera.position.y = 0; //
-        // camera.position.z = 100;//500 * Math.cos(Date.now() / 2000); //100; //
+        // camera.position.x = 500 * Math.sin(Math.PI * 1);
+        // camera.position.y = 0;
+        // camera.position.z = 500 * Math.cos(Math.PI * 1);
         //原点方向を見つめる
         camera.lookAt(new THREE.Vector3(0, 0, 0));
 
